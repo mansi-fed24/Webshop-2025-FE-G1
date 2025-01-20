e73b420cdd203d9a845af3f8646f9d26e8339404
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-01-20 14:42:06 +0100

    add dynamic route handler

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,24 @@
-export async function fetchProducts(endpoint = "/api/products") {
-  // Simulating network delay
+
+export function getBaseUrl() {
+  // Get the group number from the hostname to determine the base URL for BE
+  const regex = /webshop\-2025\-(g[0-9]{1,2})\-fe/g;
+  const href = window.location.href;
+  const match = regex.exec(href);
+  console.log(match);
+  if (match) {
+    const group = match[1];
+    return `https://webshop-2025-${group}-be.vercel.app/`;
+  }
+  return "http://localhost:3000/";
+}
+
+export async function fetchProducts(endpoint = "api/products") {
   //! DONT USE THIS IN PRODUCTION
-  return new Promise((resolve, reject) => {
-    fetch(`http://localhost:3000${endpoint}`)
-      .then((response) => response.json())
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
-  });
+  const url = `${getBaseUrl()}${endpoint}`;
+  const response = await fetch(url);
+  if(response.ok){
+    const data = await response.json();
+    return data;
+  }
+  return [];    
 }
