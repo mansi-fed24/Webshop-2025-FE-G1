f31b5f07f91b4b23660904179d94374a2faed9fd
Mansi <mansijoshiin80@gmail.com>
2025-03-21 13:35:29 +0100

    Change the getbaseurl function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,22 @@
 
+// export function getBaseUrl() {
+//   // Get the group number from the hostname to determine the base URL for BE
+//   const regex = /webshop\-2025\-(g[0-9]{1,2})\-fe/g;
+//   const href = window.location.href;
+//   const match = regex.exec(href);
+//   console.log(match);
+//   if (match) {
+//     const group = match[1];
+//     return `https://webshop-2025-be-g1-blush.vercel.app/`;
+  
+//     }
+//   return "http://localhost:3000/";
+// }
+
 export function getBaseUrl() {
-  // Get the group number from the hostname to determine the base URL for BE
-  const regex = /webshop\-2025\-(g[0-9]{1,2})\-fe/g;
-  const href = window.location.href;
-  const match = regex.exec(href);
-  console.log(match);
-  if (match) {
-    const group = match[1];
-    return `https://webshop-2025-be-g1-blush.vercel.app/`;
-    }
+  if (!window.location.href.includes('localhost')) {
+    return "https://webshop-2025-be-g1-blush.vercel.app/";
+  }
   return "http://localhost:3000/";
 }
 
