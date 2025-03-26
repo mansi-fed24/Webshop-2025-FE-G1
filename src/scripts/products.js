fa21d8bfa8a8c203f098b3519a974ad0419e22c0
Fenris Janghede <fenrisjanghede@outlook.com>
2025-03-26 14:36:52 +0100

    created css for products with img styling

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -31,7 +31,8 @@ function createProductCard(product) {
 	element.className = "product-card";
 
 	element.innerHTML = `
-		<img src="https://tinypic.host/images/2025/03/26/placeholder-img-160x160.png">
+	
+		<img src="${product.image}" alt="${product.name}">
     <h3>${product.name}</h3>
 		<h4><i> ${product.brand}</i>, ${product.amount} ${product.unit}</h4>
     <p>${product.price.toFixed(2)} kr</p>
@@ -51,26 +52,3 @@ function createProductCard(product) {
 
 	return element;
 }
-
-// document.addEventListener("DOMContentLoaded", () => {
-// 	const productsContainer = document.getElementById("products");
-
-// 	fetch("https://webshop-2025-be-g1-blush.vercel.app/api/products")
-// 		.then((response) => response.json())
-// 		.then((products) => {
-// 			products.forEach((product) => {
-// 				const productCard = document.createElement("div");
-// 				productCard.classList.add("product");
-
-// 				productCard.innerHTML = `
-//                     <img src="${product.image}" alt="${product.name}">
-//                     <h2>${product.name}</h2>
-//                     <p>${product.description}</p>
-//                     <span>Price: $${product.price}</span>
-//                 `;
-
-// 				productsContainer.appendChild(productCard);
-// 			});
-// 		})
-// 		.catch((error) => console.error("Error fetching products:", error));
-// });
